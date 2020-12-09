2ff5f91688fb72b9187bd3f8ce2f23c59dbc00e6
cambrown101 <cameron-brown@live.com>
2020-12-09 14:45:21 -0800

    Add update Index bringing in components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'components/Appointment/styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
+}
